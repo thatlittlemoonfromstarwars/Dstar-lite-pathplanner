e2054288cd4e01a669ca0e389e802ddd39e517d1
Andor Siegers <andor.siegers@gmail.com>
2024-07-16 16:37:25 -0600

    Fixed circle tracing function

--- python/python/rad_inflation.py
+++ python/python/rad_inflation.py
@@ -2,9 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import copy
+import signal
+import sys
 
-ARC_DENSITY = 1 # number of points along each arc for every 10 degrees (or 0.1745 radians)
-
+ARC_DENSITY = 20 # points per unit length
+MIN_TURN_RADIUS = 3
 class Circle:
     def __init__(self, center, rad):
         self.center = center
@@ -106,158 +108,133 @@ def find_center_and_rad(p1, p2, p3):
 
     # calculate radius
     rad = distance((a, b), p1)
-    print("Center: " + str((a,b)) + " Rad: " + str(rad))
 
     return (a, b), rad
-
-def get_points_along_rad(circle, p1, p2, arc_density):
-    # draws points along the given section of the circle
-
-    points = [p1]
-
+        
+def draw_points_on_circle(circle, point1, point2, density):
     center = circle.center
-    rad = circle.rad
+    radius = circle.rad
 
-    cx = center[0]
-    cy = center[1]
-
-    # find angle from p1 to p2
-    angle = angle_between_points(p1, center, p2)
-
-    # determine the number of points based on the specified arc density
-    num_points = round(angle * arc_density / 0.1745) # 0.1745 radians ≈ 10 degrees
-
-    # calculate the incremental angle between each point
-    sub_angle = angle / num_points
-    temp_angle = sub_angle
-
-    angle_from_p1_to_y_axis = angle_between_points(p1, center, (center[0], 1))
-    if angle_from_p1_to_y_axis > np.pi / 2:
-        angle_from_p1_to_y_axis = np.pi - angle_from_p1_to_y_axis
+    def angle_from_center(center, point):
+        # Calculate the angle from the point to the positive x-axis
+        return math.atan2(point[1] - center[1], point[0] - center[0])
     
-    angle_from_p1_to_x_axis = np.pi / 4 - angle_from_p1_to_y_axis
-
-    # iterate to calculate each point along the arc
-    while temp_angle < angle:
-        # calculate x coordinate relative to center
-        delta_x = rad * math.cos(temp_angle)
-        if p1[0] > cx and p2[0] > cx: # fully in quadrant 1 or 4 (where origin is center)
-            x = cx + delta_x
-        elif p1[0] < cx and p2[0] < cx: # fully in quadrant 2 or 3 
-            x = cx - delta_x
-        else: # starts and ends on different sides of the y-axis
-            if temp_angle < angle_from_p1_to_y_axis: # point left of y-axis
-                x = cx - delta_x
-            else: # point right of x-axis
-                x = cx + delta_x
-
-        # calculate y coordinate relative to center
-        delta_y = rad * math.sin(temp_angle)
-        if p1[1] > cy and p2[1] > cy: # fully in quadrant 1 or 2
-            y = cy + delta_y
-        elif p1[1] < cy and p2[1] < cy: # fully in quadrant 3 or 4
-            y = cy - delta_y
-        else: # starts and ends on different sides of the x-axis
-            if temp_angle < angle_from_p1_to_x_axis: # point below x-axis
-                y = cy - delta_y
-            else: # point above y-axis
-                y = cy + delta_y
-        
-        # add points to list
-        points.append((x, y))
-
-        temp_angle += sub_angle
+    def interpolate_points(angle_start, angle_end, num_points):
+        # Generate points along the arc between angle_start and angle_end
+        return [(center[0] + radius * math.cos(angle_start + i * (angle_end - angle_start) / num_points),
+                 center[1] + radius * math.sin(angle_start + i * (angle_end - angle_start) / num_points))
+                for i in range(num_points)]
     
+    # Calculate angles for the two points relative to the center
+    angle1 = angle_from_center(center, point1)
+    angle2 = angle_from_center(center, point2)
+
+    # Ensure the angles are in the range [0, 2*pi]
+    if angle1 < 0:
+        angle1 += 2 * math.pi
+    if angle2 < 0:
+        angle2 += 2 * math.pi
+
+    # Calculate the clockwise and counterclockwise paths
+    clockwise_path = (angle2 - angle1) % (2 * math.pi)
+    counterclockwise_path = (angle1 - angle2) % (2 * math.pi)
+
+    # Determine the shorter path and interpolate points accordingly
+    if clockwise_path <= counterclockwise_path:
+        num_points = int(clockwise_path * density / (2 * math.pi) * radius)
+        points = interpolate_points(angle1, angle1 + clockwise_path, num_points)
+    else:
+        num_points = int(counterclockwise_path * density / (2 * math.pi) * radius)
+        points = interpolate_points(angle2, angle2 + counterclockwise_path, num_points)
+        points = points[::-1]  # Reverse to go from point1 to point2
+
     return points
-    
 
-        
 def adjust_path_for_turn_radius(input_path, min_turn_radius):
     og_path = copy.deepcopy(input_path)
 
     altered_path = [og_path[0]]
     circles = []
+    last_ind = 0
     for i in range(1, len(og_path) - 1):
         p1 = og_path[i - 1]
         p2 = og_path[i]
         p3 = og_path[i + 1]
 
         angle = angle_between_points(p1, p2, p3)
-        print(angle)
         if angle != 0 and angle < np.pi / 2:  # Detect sharp turns
             # find a circle that includes all three points
             center, radius = find_center_and_rad(p1, p2, p3)
             if center is not None and radius < min_turn_radius:
                 temp_circle = Circle(center, radius)
-                print("Circle at: " + str(temp_circle.center) + " with rad: " + str(temp_circle.rad))
                 circles.append(temp_circle)
-
+                print("Drawing arc from point: " + str(len(altered_path) - 1))
                 # find new points along circle to connect p1 to p3
-                new_points = get_points_along_rad(temp_circle, p1, p3, ARC_DENSITY)
-                og_path[i] = new_points[-2]
+                new_points = draw_points_on_circle(temp_circle, p1, p3, ARC_DENSITY)
+                og_path[i] = new_points[-1]
 
                 altered_path.extend(new_points)
-
             else:
                 altered_path.append(p2)
-
         else:
             altered_path.append(p2)
+        
+        # search for and remove duplicate points
+        i = last_ind
+        while(i < len(altered_path) - 1):
+            if altered_path[i] == altered_path[i+1]:
+                altered_path.pop(i)
+            else:
+                i += 1
+        last_ind = i - 1
 
     altered_path.append(og_path[-1])
+
     return altered_path, circles
 
 # Example usage
-# csv_data = """41,54
-# 40,55
-# 39,55
-# 38,56
-# 37,57
-# 36,58
-# 35,58
-# 34,58
-# 33,58
-# 32,58
-# 31,58
-# 30,59
-# 31,60
-# 32,60
-# 33,60
-# 34,60
-# 35,60
-# 36,60
-# 37,61
-# 38,62
-# 39,63
-# 40,63
-# 41,63
-# 42,63
-# """
-
-csv_data = """51,51
-51,52
-51,53
-51,54
-51,55
-51,56
-51,57
-51,58
-51,59
-50,60
-49,61
-48,62
-48,63
-48,64
-48,65
+csv_data = """41,54
+40,55
+39,55
+38,56
+37,57
+36,58
+35,58
+34,58
+33,58
+32,58
+31,58
+30,59
+31,60
+32,60
+33,60
+34,60
+35,60
+36,60
+37,61
+38,62
+39,63
+40,63
+41,63
+42,63
+41,64
 """
 
-# Example usage
+# csv_data = """51,57
+# 51,59
+# 50,60
+# 49,61
+# 48,62
+# 48,63
+# 48,64
+# 48,65
+# """
+
 input_path = read_csv_data(csv_data)
-min_turn_radius = 5
 
 figure, axes = plt.subplots()
 
-altered_path, circles = adjust_path_for_turn_radius(input_path, min_turn_radius)
+altered_path, circles = adjust_path_for_turn_radius(input_path, MIN_TURN_RADIUS)
 
 for circle in circles:
     temp_circle = plt.Circle(circle.center, circle.rad)
@@ -272,6 +249,15 @@ altered_path = np.array(altered_path)
 
 plt.plot(input_path[:, 0], input_path[:, 1], 'bo-', label='Input Path')
 plt.plot(altered_path[:, 0], altered_path[:, 1], 'ro-', label='Altered Path')
+
+# Annotate the input path points
+# for i, (x, y) in enumerate(input_path):
+#     plt.annotate(str(i), (x, y), textcoords="offset points", xytext=(5, 5), ha='center')
+
+# Annotate the altered path points
+for i, (x, y) in enumerate(altered_path):
+    plt.annotate(str(i), (x, y), textcoords="offset points", xytext=(5, 5), ha='center')
+
 plt.legend()
 plt.xlabel('X')
 plt.ylabel('Y')
